15849547262d2af6e0fc7aa3c60a32cd0d064622
fc1054547159 <fabiocruzlozano@gmail.com>
2023-02-14 22:04:34 -0500

    se inicio proyecto para posteriormente conectar el login

---
+++ src/MovieCard.jsx
@@ -0,0 +1,11 @@
+import styles from "./MovieCard.module.css";
+
+export function MovieCard({ movie }) {
+    const imageUrl = "http://image.tmdb.org/t/p/w300" + movie.poster_path;
+    return (
+        <li className={styles.movieCard}>
+            <img className={styles.movieImage}  src={imageUrl} alt={movie.title} />
+            <div>{movie.title}</div>
+        </li>
+    );
+}
